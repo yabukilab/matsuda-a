da8855d8bfc8a654f7d0b0c3b53121cb2dc8c927
Mazeru <s2242066mx@s.chibakoudai.jp>
2024-07-05 01:02:13 +0900

    aa

--- htdocs/touroku.php
+++ htdocs/touroku.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>商品管理</title>
-    <link rel="stylesheet" href="a.css">
+    <link rel="stylesheet" href="css">
 </head>
 <body>
     <div class="header"></div>
