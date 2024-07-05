9f41b1dba3aba6044d6d59bdcb9f09c52611c26a
Mazeru <s2242066mx@s.chibakoudai.jp>
2024-07-05 16:24:37 +0900

    z

--- htdocs/comment.php
+++ htdocs/comment.php
@@ -28,7 +28,7 @@
 
         if ($rec == false) {
             print '商品コードが正しくありません。';
-            print '<a> href="index.php">戻る</a>';
+            print '<az href="index.php">戻る</a>';
             exit();
         }
 
